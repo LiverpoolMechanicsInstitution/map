1683b9595d2308f41cd383bd40c78c66c5420046
Tomas Vancisin <tomasvancisin@gmail.com>
2026-05-19 22:09:07 +0100

    copyright

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,22 @@
 <script>
   import Map from "./lib/Map.svelte";
+  const currentYear = new Date().getFullYear();
 </script>
 
+<p>&copy; {currentYear} Isabel Robinson</p>
 <Map />
+
+<style>
+  p {
+    border-radius: 3px;
+    margin: 0;
+    padding: 5px 5px;
+    background-color: black;
+    position: fixed;
+    bottom: 0px;
+    left: 50%;
+    font-weight: 600;
+    transform: translateX(-50%);
+    z-index: 999;
+  }
+</style>
